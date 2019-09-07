cbb2fd14c98742145f10ab4e51e83fe990e977ec
Roman Pidkostelnyi <Pidkostelnyi Roman>
2019-09-07 16:03:43 +0300

    created second template

--- script.js
+++ script.js
@@ -2,14 +2,19 @@ const pug = require('pug');
 const fs = require('fs');
 const data = require('./data.json');
 
-const compiledFunction = pug.compileFile('index.pug');
+const compiledFunction1 = pug.compileFile('index.pug');
+const compiledFunction2 = pug.compileFile('index2.pug');
 
-fs.writeFile("./index.html", compiledFunction({data: data}), (e) => {
+fs.writeFile("./index.html", compiledFunction1({data: data}), (e) => {
     if(e) {
-        return console.log(e);
+        console.log(e);
     }
-    console.log("The file was saved!");
+    console.log("index was saved!");
+});
+fs.writeFile("./index2.html", compiledFunction2({data: data}), (e) => {
+    if(e) {
+        console.log(e);
+    }
+    console.log("index2 was saved!");
 });
 
-
-console.log();
